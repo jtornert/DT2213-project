921a207a3c256db89ed9ae32cbf4679888bea80d
jtornert <joakim.tornert@outlook.com>
2022-05-20 12:46:25 +0200

    working tension and drag

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const audioCtx = new AudioContext();
 
+const samples = new Array(4);
+const pos = {
+  x: 0,
+  y: 0,
+  dx: 0,
+  dy: 0,
+};
+
+let box;
+
+(async function () {
+  const bufferV1 = await fetchAudioBuffer("saw_V1.wav");
+  const bufferV2 = await fetchAudioBuffer("saw_V2.wav");
+  const bufferV3 = await fetchAudioBuffer("saw_V3.wav");
+  const bufferV4 = await fetchAudioBuffer("saw_V4.wav");
+
+  samples[0] = bufferV1;
+  samples[1] = bufferV2;
+  samples[2] = bufferV3;
+  samples[3] = bufferV4;
+
+  box = document.getElementById("box");
+
+  box.addEventListener("mousedown", handleMouseDown);
+})();
+
 async function fetchAudioBuffer(url) {
   return fetch(new Request(url))
     .then((response) => response.arrayBuffer())
@@ -15,34 +41,38 @@ function play(buffer) {
   source.start(0);
 }
 
-(async function () {
-  const buttonV1 = document.getElementById("v1");
-  const buttonV2 = document.getElementById("v2");
-  const buttonV3 = document.getElementById("v3");
-  const buttonV4 = document.getElementById("v4");
+function handleMouseDown(event) {
+  console.log("Enable drag");
+  pos.x = event.clientX;
+  pos.y = event.clientY;
+  window.addEventListener("mousemove", handleMouseMove);
+  window.addEventListener("mouseup", handleMouseUp);
+}
 
-  const bufferV1 = await fetchAudioBuffer("saw_V1.wav");
-  const bufferV2 = await fetchAudioBuffer("saw_V2.wav");
-  const bufferV3 = await fetchAudioBuffer("saw_V3.wav");
-  const bufferV4 = await fetchAudioBuffer("saw_V4.wav");
-  // const bufferV1 = await fetchAudioBuffer("loop.wav");
-  // const bufferV2 = await fetchAudioBuffer("loop.wav");
-  // const bufferV3 = await fetchAudioBuffer("loop.wav");
-  // const bufferV4 = await fetchAudioBuffer("loop.wav");
+function handleMouseMove(event) {
+  console.log("Moving");
+  pos.dx = event.clientX - pos.x;
+  pos.dy = event.clientY - pos.y;
+  console.log(pos.dx, pos.dy);
+  box.style.transform = `translate(
+    ${Math.atan(pos.dx / 500) * 250}px,
+    ${Math.atan(pos.dy / 500) * 100}px
+  )`;
+}
 
-  buttonV1.addEventListener("click", () => {
-    play(bufferV1);
-  });
-
-  buttonV2.addEventListener("click", () => {
-    play(bufferV2);
-  });
-
-  buttonV3.addEventListener("click", () => {
-    play(bufferV3);
-  });
-
-  buttonV4.addEventListener("click", () => {
-    play(bufferV4);
-  });
-})();
+function handleMouseUp(event) {
+  console.log("Disable drag");
+  console.log(pos.dx, pos.dy);
+  if (Math.abs(pos.dx) > 500) {
+    play(samples[3]);
+  } else if (Math.abs(pos.dx) > 400) {
+    play(samples[2]);
+  } else if (Math.abs(pos.dx) > 300) {
+    play(samples[1]);
+  } else if (Math.abs(pos.dx) < 300) {
+    play(samples[0]);
+  }
+  box.style.transform = `translate(${0}px, ${0}px)`;
+  window.removeEventListener("mousemove", handleMouseMove);
+  window.removeEventListener("mouseup", handleMouseUp);
+}
